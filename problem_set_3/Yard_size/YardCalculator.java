9e5e8965938bbfb305758cfebac49836b592c711
Alejandro Pineda <alejandro.deathscythe@gmail.com>
2014-05-29 18:39:40 -0500

    PS 3 - Iniciado
    Iniciado Problem Set 3

--- problem_set_3/Yard_size/YardCalculator.java
+++ problem_set_3/Yard_size/YardCalculator.java
@@ -1,4 +1,38 @@
+
+//A landscape designer needs a program to make calculations in meters about a front yard given its width and length in feet. 
+//Write a class YardCalculator with a constructor that takes the width and length in feet as parameters
+//   public YardCalculator(double theLength, double theWidth)
+
+//Provide methods: 
+
+//   public double perimeter() which returns the perimeter in meters
+//   public double area() which returns the area in square meters
+//   public double diagonal() which returns the diagonal in meters
+
+//1 foot = 0.3048 meters.
+
+
+//Define and use a constant for the conversion factor. Define it in the class itself, outside any method, 
+//so all the methods can use it.
+
+//      public static  final double METERS_PER_FOOT = 0.3048;
+
+//For the draft, provide the instance variables and the constructor. Initialize  the instance variables in the constructor
+
 public class YardCalculator
 {
-   ///your code here
+   //instance variables here
+   
+   /**
+    * Constructs a YardCalculator with the given parameters
+    * @param theLength the length of the yard
+    * @param theWidth the width of the yard
+    */
+   double theLength;
+   double theWidth;
+   
+   public YardCalculator(double theLength, double theWidth){
+       this.theLength=theLength;
+       this.theWidth=theWidth;
+    }
 }
--- problem_set_3/hw03_08 volume of cone/Cone.java
+++ problem_set_3/hw03_08 volume of cone/Cone.java
@@ -1,18 +1,32 @@
-//hw03_08
-/*
- * Complete the code to find the volume of a cone with radius r and height h
- */
+//
+// Complete the code to find the volume of a cone with radius r and height h.
+//Use Math.PI
+//
 public class Cone
 {
+   private int r;
+   private int h;
+
    /**
-    * Gets the volume of a cone
-    * @param r the radius of the cone
-    * @param h the height of the cone
+    * Constructor for the Cone class
+    * @param r, the radius of the cone
+    * @param h, the height of the cone
+   */
+   public Cone(int r, int h)
+   {
+      this.r = r;
+      this.h = h;
+   }
+   
+   /**
+    * Returns the volume of a cone
+    * Volume of a cone = 1/3 * PI * r^2 * h
     */
-   public double volume(int r, int h)
+   public double getVolume()
    {
        double volume;
-       //your code is here
+       // TODO write the code to assign the volume of the cone to the variable volume. Use Math.PI
+       volume=(double) (1.0/3.0*Math.PI*r*r*h);
        return volume;
    }
 }
